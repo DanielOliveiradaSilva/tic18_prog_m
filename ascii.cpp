a32fd8052df8d794802716d35bf286e4e9b0d873
daniel <danieloliveira.s558@gmail.com>
2023-08-29 12:24:05 -0300

    operadores acidionados

---
+++ ascii.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (void){
+    
+    //0-48 começa de... 9 - 57
+    
+
+    cout << "-Char-"<<"-DEC-"<< "-OCT-" <<"-HX-"<<endl;
+    for (int i = 48; i < 58; i++ ){
+        
+        cout << char(i)<<"--" <<i <<"--"<< oct<< i <<"--" <<hex << i<<endl;
+        hex2dec>>i;
+    }
+    return 0;
+
+}
