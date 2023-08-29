3abe394b6362a973d7729f13c056d87c8a471b68
daniel <danieloliveira.s558@gmail.com>
2023-08-29 10:53:57 -0300

    adicionei mais arquivos

---
+++ intTamanho.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+//verificar
+int main (void){
+   
+   cout << "";
+    return 0;
+}
---
+++ pontoFlutuante.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//literal é um double
+//literal notação cientifica
+//
+int main (void){
+    double val = 1.0/3.0;
+    long double erro = 1.0/1234556789.0;
+    
+    bool isNumber= true;
+    cout<<sizeof(isNumber)<<" bite \n";
+
+    return 0;
+}
