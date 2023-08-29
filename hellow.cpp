20faeaf24bc687b071d1352001a76a527734d65e
daniel <danieloliveira.s558@gmail.com>
2023-08-29 09:05:42 -0300

    init

---
+++ hellow.cpp
@@ -0,0 +1,12 @@
+/*Primeiro Programa em c++
+Este Programa exibe a mensagem "Hello, word!" no monitor*/ 
+
+#include <iostream>//incluido os recursos da biblioteca de entrada e saída
+//using namespace std: //usar o namespace std
+
+int main(void){//programa c++ começam executando a função main
+
+    std:: cout <<"Hello, world!\n"; //imprime mesagem na saída padrão
+
+    return 0;
+}
---
+++ idade.cpp
@@ -0,0 +1,17 @@
+//variavel espaço de memoria previamente armazenado
+
+#include<iostream>
+using namespace std;//cuidado onde está listado
+int main(void){
+
+    cout << "Qual a sua idade?:";
+    int idade;
+    /* uma instrução que reserva memoria
+        e introduz um identificador para um
+        objeto em um programa é chamado de definição de váriavel
+     */
+    cin >> idade;
+    /*O nome Cin refere-se ao fluxo de 
+    entrada padrão */
+
+}
